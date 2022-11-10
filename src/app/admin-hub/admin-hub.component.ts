748d131d6be2cd88a40c482cb339c535629b8513
adujacqu <alex.dujacquier@gmail.com>
2022-11-10 15:14:33 +0100

    add method deleteEmployee() in admin-hub.component.ts

--- src/app/admin-hub/admin-hub.component.ts
+++ src/app/admin-hub/admin-hub.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {DtoOutputCreateUser} from "../user-hub/dtos/dto-output-create-user";
 import {AdminService} from "./admin.service";
 import {DtoInputUser} from "../user-hub/dtos/dto-input-user";
+import {DtoOutputDeleteEmployee} from "./dtos/dto-output-delete-employee";
 
 @Component({
   selector: 'app-admin-hub',
@@ -26,5 +27,13 @@ export class AdminHubComponent implements OnInit {
 
   fetchAllEmployees(){
     this._adminService.fetchAllEmployees().subscribe(employees=> this.employees = employees)
+  }
+
+  deleteEmployee(dto: DtoOutputDeleteEmployee){
+    let employee = this.employees.filter(e=> e.id==dto.id)
+    let index = this.employees.indexOf(employee[0])
+    this._adminService
+      .deleteEmployee(dto)
+      .subscribe(()=>this.employees.splice(index, 1))
   }
 }
