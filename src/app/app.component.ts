f67b9059f9506b8c76e781f16026165ce1e45eb4
PierreGillebert <la206511@student.helha.be>
2022-11-14 15:30:45 +0100

    Deplacement de la gestion des boutons login et register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {DtoOutputCreateUser} from "./user-hub/dtos/dto-output-create-user";
+import {DtoOutputLogUser} from "./user-hub/dtos/dto-output-log-user";
+import {UserService} from "./user-hub/user.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Project';
+  LoginActive = false;
+  RegisterActive = false;
+  AdminActive = false;
+
+  constructor(private _userService : UserService) { }
+
+  clickLogin() {
+    this.resetDisplay();
+    this.LoginActive = true;
+  }
+
+  clickRegister() {
+    this.resetDisplay();
+    this.RegisterActive = true;
+  }
+
+  clickAdmin() {
+    this.resetDisplay();
+    this.AdminActive = true;
+  }
+
+  resetDisplay() {
+    this.RegisterActive = false;
+    this.AdminActive = false;
+    this.LoginActive = false;
+  }
+
+  createClient(dto: DtoOutputCreateUser) {
+    this._userService.createClient(dto).subscribe();
+  }
+
+  login(dto: DtoOutputLogUser) {
+    this._userService.login(dto).subscribe();
+  }
 }
--- src/app/user-hub/user-hub.component.ts
+++ src/app/user-hub/user-hub.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {DtoOutputCreateUser} from "./dtos/dto-output-create-user";
-import {UserService} from "./user.service";
-import {DtoOutputLogUser} from "./dtos/dto-output-log-user";
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-user-hub',
@@ -9,35 +6,6 @@ import {DtoOutputLogUser} from "./dtos/dto-output-log-user";
   styleUrls: ['./user-hub.component.css']
 })
 export class UserHubComponent implements OnInit {
-  LoginClick = false;
-  RegisterClick = false;
-
-  constructor(private _userService : UserService) { }
-
   ngOnInit(): void {
-  }
-
-  clickLogin() {
-    this.LoginClick = !this.LoginClick;
-
-    if (this.LoginClick) {
-      this.RegisterClick = false;
-    }
-  }
-
-  clickRegister() {
-    this.RegisterClick = !this.RegisterClick;
-
-    if (this.RegisterClick) {
-      this.LoginClick = false;
-    }
-  }
-
-  createClient(dto: DtoOutputCreateUser) {
-    this._userService.createClient(dto).subscribe();
-  }
-
-  login(dto: DtoOutputLogUser) {
-    this._userService.login(dto).subscribe();
   }
 }
