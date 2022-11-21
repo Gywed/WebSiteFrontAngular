6afa914b6ddae5c5ef233371ceb79933e7eb5490
Yohan <yohan.noel.huls@gmail.com>
2022-11-21 18:08:25 +0100

    Fix user-hub code remove

--- src/app/user-hub/user-hub.component.ts
+++ src/app/user-hub/user-hub.component.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {DtoOutputCreateUser} from "./dtos/dto-output-create-user";
+import {UserService} from "./user.service";
+import {DtoOutputLogUser} from "./dtos/dto-output-log-user";
 
 @Component({
   selector: 'app-user-hub',
@@ -7,5 +10,15 @@ import {Component, OnInit} from '@angular/core';
 })
 export class UserHubComponent implements OnInit {
   ngOnInit(): void {
+  }
+
+  constructor(private _userService : UserService) { }
+
+  createClient(dto: DtoOutputCreateUser) {
+    this._userService.createClient(dto).subscribe();
+  }
+
+  login(dto: DtoOutputLogUser) {
+    this._userService.login(dto).subscribe();
   }
 }
