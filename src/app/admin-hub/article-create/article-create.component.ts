a09d62e55290e6d981fb3de23e8581d8a3c18eea
PierreGillebert <la206511@student.helha.be>
2022-12-07 15:48:50 +0100

    Replace the number visual of the categories with their names in the article create form

--- src/app/admin-hub/article-create/article-create.component.ts
+++ src/app/admin-hub/article-create/article-create.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {DtoOutputCreateArticle} from "../../article-hub/dtos/dto-output-create-article";
+import {DtoInputCategory} from "../../order-hub/dtos/dto-input-category";
 
 @Component({
   selector: 'app-article-create',
@@ -14,12 +15,17 @@ export class ArticleCreateComponent implements OnInit {
     price : ['', Validators.required],
     pricingType : ['', Validators.required],
     stock : ['', Validators.required],
-    idCategory : ['', Validators.required],
     idBrand : ['', Validators.required]
   })
 
+  idCategory = 0;
+  idBrand = 0;
+  PricingType = 0;
+
   @Output()
   articleCreated: EventEmitter<DtoOutputCreateArticle> = new EventEmitter<DtoOutputCreateArticle>()
+
+  @Input() listOfCategories: DtoInputCategory[] = []
 
   constructor(private _fb: FormBuilder) { }
 
@@ -36,9 +42,21 @@ export class ArticleCreateComponent implements OnInit {
       price : this.form.value.price,
       pricingtype : this.form.value.pricingType,
       stock : this.form.value.stock,
-      idcategory : this.form.value.idCategory,
+      idcategory : this.idCategory,
       idbrand : this.form.value.idBrand
     })
     this.form.reset();
+  }
+
+  setIdCategory(id: any) {
+    this.idCategory = id.target.value;
+  }
+
+  setIdBrand(id: number) {
+    this.idBrand = id;
+  }
+
+  setPricingType(id: number) {
+    this.PricingType = id;
   }
 }
