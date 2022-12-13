f33882a7090481206c3d2eb96c17ecffbc0a44e2
Yohan <yohan.noel.huls@gmail.com>
2022-12-13 15:42:01 +0100

    Add order-list-date.component to routing

--- src/app/order-hub/order-list-date/order-list-date.component.ts
+++ src/app/order-hub/order-list-date/order-list-date.component.ts
@@ -2,6 +2,8 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {DtoInputOrder} from "../dtos/dto-input-order";
 import {DtoOutputOrderDate} from "../dtos/dto-output-order-date";
 import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {EmitEvent, EventBusService, Events} from "../../event-bus.service";
+import {DtoInputOrderContent} from "../dtos/dto-input-order-content";
 
 @Component({
   selector: 'app-order-list-date',
@@ -9,19 +11,18 @@ import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/form
   styleUrls: ['./order-list-date.component.css']
 })
 export class OrderListDateComponent implements OnInit {
-  @Input()
   orders : DtoInputOrder[] = []
-
-  @Output()
-  dateForFetch : EventEmitter<DtoOutputOrderDate> = new EventEmitter<DtoOutputOrderDate>();
 
   form: FormGroup = this._fb.group({
     date : ['', Validators.required],
   })
 
-  constructor(private _fb : FormBuilder) { }
+  constructor(private _fb : FormBuilder,
+              private _eventBus: EventBusService) { }
 
   ngOnInit(): void {
+    this._eventBus.on(Events.fetchOrderByDate).
+    subscribe(orders => this.orders = orders);
   }
 
   controls(name: string): AbstractControl | null {
@@ -29,8 +30,19 @@ export class OrderListDateComponent implements OnInit {
   }
 
   emitDate() {
-    this.dateForFetch.next({
-        date : this.form.value.date
-    });
+    this._eventBus.emit(new EmitEvent(Events.fetchOrderByDate, {
+      date : this.form.value.date
+    }))
+    this.form.reset();
+  }
+
+  emitPreparedUpdate(order: DtoInputOrder, orderContent: DtoInputOrderContent) {
+    this._eventBus.emit(new EmitEvent(Events.updateOrderContent, {
+      orderid : order.id,
+      articleid : orderContent.article.id,
+      prepared : !orderContent.prepared
+    }));
+    orderContent.prepared = !orderContent.prepared
+    order.isFullyPrepared = order.orderContents.every(o => o.prepared)
   }
 }
