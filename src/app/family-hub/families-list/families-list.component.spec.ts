99aaa55ae63e7be0d0e072e709c469fa32b0d0f3
adujacqu <alex.dujacquier@gmail.com>
2022-12-15 17:54:44 +0100

    create families-list-component

---
+++ src/app/family-hub/families-list/families-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FamiliesListComponent } from './families-list.component';
+
+describe('FamiliesListComponent', () => {
+  let component: FamiliesListComponent;
+  let fixture: ComponentFixture<FamiliesListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FamiliesListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FamiliesListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
