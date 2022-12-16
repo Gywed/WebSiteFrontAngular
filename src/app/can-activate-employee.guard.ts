912930f4ff0074d6a22c07e704afc9fe281d6471
Yohan <yohan.noel.huls@gmail.com>
2022-12-16 10:08:08 +0100

    Create can-activate-employee.guard

---
+++ src/app/can-activate-employee.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import {AppAuthService} from "./app-auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CanActivateEmployeeGuard implements CanActivate, CanActivateChild {
+  constructor(private _authService : AppAuthService,
+              private _router : Router) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+
+}
