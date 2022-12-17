b8b146b47339a08004c78010f72ef13d63693c87
adujacqu <alex.dujacquier@gmail.com>
2022-12-17 11:59:14 +0100

    add deleteFamily() for family-hub.component.ts using family.service.ts

--- src/app/family-hub/family-hub.component.ts
+++ src/app/family-hub/family-hub.component.ts
@@ -4,6 +4,7 @@ import {DtoInputFamily} from "./dtos/dto-input-family";
 import {EmitEvent, EventBusService, Events} from "../event-bus.service";
 import {DtoInputArticle} from "../dtos/dto-input-article";
 import {DtoOutputCreateFamily} from "./dtos/dto-output-create-family";
+import {DtoOutputDeleteFamily} from "./dtos/dto-output-delete-family";
 
 @Component({
   selector: 'app-family-hub',
@@ -37,6 +38,13 @@ export class FamilyHubComponent implements OnInit {
 
   createFamily(dto: DtoOutputCreateFamily){
     this._familyService.createFamily(dto).subscribe(family => this.families.push(family))
+  }
+
+  deleteFamily(dto:DtoOutputDeleteFamily){
+    let family = this.families.filter(e => e.id == dto.id)
+    let index = this.families.indexOf(family[0])
+    this._familyService.deleteFamily(dto)
+      .subscribe(()=> this.families.splice(index, 1))
   }
 
 }
--- src/app/family-hub/family.service.ts
+++ src/app/family-hub/family.service.ts
@@ -5,6 +5,7 @@ import {Observable} from "rxjs";
 import {DtoInputFamily} from "./dtos/dto-input-family";
 import {DtoInputArticle} from "../dtos/dto-input-article";
 import {DtoOutputCreateFamily} from "./dtos/dto-output-create-family";
+import {DtoOutputDeleteFamily} from "./dtos/dto-output-delete-family";
 
 @Injectable({
   providedIn: 'root'
@@ -25,5 +26,9 @@ export class FamilyService {
 
   createFamily(dto: DtoOutputCreateFamily):Observable<DtoInputFamily>{
     return this._httpClient.post<DtoInputFamily>(FamilyService.ENTRY_POINT_FAMILY+"/create", dto)
+  }
+
+  deleteFamily(dto: DtoOutputDeleteFamily):Observable<DtoInputFamily>{
+    return this._httpClient.request<any>('delete', FamilyService.ENTRY_POINT_FAMILY+"/delete", {body: dto})
   }
 }
